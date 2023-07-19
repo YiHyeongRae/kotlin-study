f0c63a07056017ab07c3dfed0cf569987349cae9
YiHyeongRae <dev.yihr@gmail.com>
2023-07-19 17:23:27 +0900

    Example3 Complete

--- app/src/main/java/com/example/kotlin/Example3.kt
+++ app/src/main/java/com/example/kotlin/Example3.kt
@@ -3,24 +3,26 @@ package com.example.kotlin
 fun main(){
 
     val user = User("이형래")
-    val kid = Kid(name = "이형곤",age=25);
+    Kid(name = "이형곤",age=25,gender="male")
 
 
-    println("내 이름은 ${user.name}");
-    println("내 나이는 ${user.age}");
-    println("kid ${kid.name},${kid.age},${kid.gender}")
+    println("내 이름은 ${user.name}")
+    println("내 나이는 ${user.age}")
+
+//    println("kid ${kid.name},${kid.age},${kid.gender}")
 
 }
 
 open class User(open val name:String, open var age:Int = 32)
 
 class Kid (override val name: String,override var age: Int, ) : User(name,age){
-    var gender :String = "male"
+    var gender :String = "female"
 
     init{
-        println("정보 제공 받는 중 입니다.")
+        println("초기화 중 입니다.")
     }
     constructor(name:String, age:Int, gender:String) : this(name,age){
     this.gender = gender
+        println("부 생성자 호출")
     }
 }
