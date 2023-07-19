a0e59b3d8dcce147e714a3d76168d9cf6a355e61
YiHyeongRae <dev.yihr@gmail.com>
2023-07-19 18:54:56 +0900

    Null Complete

---
+++ app/src/main/java/com/example/kotlin/Null.kt
@@ -0,0 +1,29 @@
+package com.example.kotlin
+
+fun main(){
+    var name : String = "이름"
+//     name = null ::: 에러
+
+    var number : Int = 10
+//    number = null ::: 에러
+
+    var nickname :String? = "율무"
+    var secondNumber :Int? = null
+
+//    ? ::: Nullable , Null 가능
+    val result = if(nickname == null){
+        "값이 없음"
+    }else{
+        nickname
+    }
+    nickname = null
+
+//    엘비스 오퍼레이터
+    val result2 = nickname?: "값이 없음"
+    println(result)
+    println(result2)
+
+//    옵셔널체이닝 느낌
+    val size = nickname?.length
+    println(size)
+}
