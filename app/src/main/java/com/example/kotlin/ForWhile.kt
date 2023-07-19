ab3bd2ed8dde7ba35107979ae6e16061fdba06ca
YiHyeongRae <dev.yihr@gmail.com>
2023-07-19 18:02:04 +0900

    ForWhile Complete

---
+++ app/src/main/java/com/example/kotlin/ForWhile.kt
@@ -0,0 +1,37 @@
+package com.example.kotlin
+
+fun main(){
+
+    1..10 == IntRange(1,10)
+    for(i in IntRange(1,10)){
+        print(i)
+        print(",")
+    }
+    println()
+    for(i in 1 until 10){
+        print(i)
+        print(",")
+    }
+    println()
+    for(i in 1..10 step(2)){
+        print(i)
+        print(",")
+    }
+    println()
+    for(i in 10 downTo 1){
+        print(i)
+        print(",")
+    }
+    println()
+    for(i in 10 downTo 1 step(2)){
+        print(i)
+        print(",")
+    }
+    println()
+    var w = 1
+    while(w < 11){
+        print("w${w}")
+        print(",")
+        w++
+    }
+}
